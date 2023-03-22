8dd397206af8bd30ae44bef937c05d8d72c3ca72
kennedybm <kennedy_muniz@hotmail.com>
2023-03-21 21:15:28 -0300

    Feat: created home folder with initial page structure and style

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+const Home = () => {
+  return (
+    <S.Main>
+      <p>Teste Front End</p>
+    </S.Main>
+  );
+};
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  background-color: #000;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  p {
+    color: yellow;
+    font-size: 4rem;
+  }
+`;

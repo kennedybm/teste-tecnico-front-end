d9c553de2b995f63b2d32c4d83279002eb37a590
kennedybm <kennedy_muniz@hotmail.com>
2023-03-22 17:22:17 -0300

    Feat: added UsersProvider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 import { PostsProvider } from "./Api/Posts";
+import { UsersProvider } from "./Api/Users";
 
 export interface IProvidersDefault {
   children: ReactNode;
 }
 
 const Providers = ({ children }: IProvidersDefault) => {
-  return <PostsProvider>{children}</PostsProvider>;
+  return (
+    <PostsProvider>
+      <UsersProvider>{children}</UsersProvider>
+    </PostsProvider>
+  );
 };
 export default Providers;

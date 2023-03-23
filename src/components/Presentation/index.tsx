3dd689b1bbd5e3d3f90225f4e4a03c63c2a9fa14
kennedybm <kennedy_muniz@hotmail.com>
2023-03-23 17:36:06 -0300

    Feat: cretated component to remove all his code from Home

---
+++ src/components/Presentation/index.tsx
@@ -0,0 +1,19 @@
+import * as S from "./styles";
+
+export const Presentaion = () => {
+  return (
+    <>
+      <S.PresentationSection>
+        <S.PresentationTitle>
+          <h2>Se mantenha atualizado</h2>
+        </S.PresentationTitle>
+        <S.PresentationDesc>
+          <p>
+            Descubra histórias, pensamentos e conhecimento de escritores de
+            quaquer lugar do mundo, sobre qualquer assunto.
+          </p>
+        </S.PresentationDesc>
+      </S.PresentationSection>
+    </>
+  );
+};

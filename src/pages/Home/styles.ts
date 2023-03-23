5f47b2920c5c7dbdfc43bd758402640d3679bb2e
kennedybm <kennedy_muniz@hotmail.com>
2023-03-23 17:18:09 -0300

    Feat: fixed logic inside handleScroll, to set in state whem the scroll is at 240

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,23 +3,56 @@ import styled from "styled-components";
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
-  background-color: #000;
-  height: 100vh;
+  background-color: var(--default);
   align-items: center;
   justify-content: center;
+  margin: 0 auto;
+`;
+
+export const PresentationSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--decor);
+  height: 15em;
+  width: 100%;
+`;
+
+export const PresentationTitle = styled.div`
+  display: flex;
+  align-items: flex-end;
+  border: 1px solid #000;
+  height: 40%;
+  width: 100%;
+  h2 {
+    font-size: var(--fs-lrg);
+    padding: 0px 0 0.1em 0.5em;
+  }
+`;
+
+export const PresentationDesc = styled.div`
+  display: flex;
+  border: 1px solid #000;
+  /* height: 50%; */
+  width: 100%;
   p {
-    color: yellow;
-    font-size: 4rem;
+    font-size: var(--fs-sml);
+    padding-top: 0.3rem;
+    width: 65%;
+    max-width: 20rem;
+    padding-left: 1em;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
-  h1 {
-    color: yellow;
-    font-size: 4rem;
-  }
-  button {
-    color: blue;
-    background-color: gray;
-    width: 4.5rem;
-    height: 2rem;
-    border-radius: 5px;
-  }
+`;
+
+export const TesteTwo = styled.div`
+  display: flex;
+  background-color: black;
+  height: 40em;
+  width: 100%;
 `;

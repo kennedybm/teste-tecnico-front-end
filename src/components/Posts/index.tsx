7cc80eb989eab3e6833dfe55517e020c2c263d72
kennedybm <kennedy_muniz@hotmail.com>
2023-03-24 16:32:18 -0300

    Feat: added Pagination components, and one new section to put then

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { PostsCard } from "./PostsCard";
 import { usePosts } from "../../providers/Api/Posts";
 import * as Styles from "./styles";
+import { Pagination } from "../Pagination";
 
 export const Posts = () => {
   const { postsData } = usePosts();
@@ -24,24 +26,97 @@ export const Posts = () => {
       title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
       body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
     },
+    {
+      userId: 1,
+      id: 4,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 5,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 6,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 7,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+      userId: 1,
+      id: 8,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+      userId: 1,
+      id: 9,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 10,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 11,
+      title: "aaaaaaaaaaaaaaaaaa AAAAAA AAAAAA",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      id: 12,
+      title: "some text here",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
   ];
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const postsPerPage = 6;
+  const viewedPosts = pageNumber * postsPerPage;
+
+  const displayPosts = posts.slice(viewedPosts, viewedPosts + postsPerPage);
+  const pageCount = Math.ceil(posts.length / postsPerPage);
+
+  const switchPage = ({ selected }: { selected: number }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <>
-      <Styles.PostsSection>
-        {posts
-          ? posts.map((item, index) => {
-              return (
-                <PostsCard
-                  key={index}
-                  userId={item.userId}
-                  id={item.id}
-                  title={item.title}
-                  body={item.body}
-                />
-              );
-            })
-          : null}
-      </Styles.PostsSection>
+      <Styles.MainSection>
+        <Styles.PostsSection>
+          {displayPosts
+            ? displayPosts.map((item, index) => {
+                return (
+                  <PostsCard
+                    key={index}
+                    userId={item.userId}
+                    id={item.id}
+                    title={item.title}
+                    body={item.body}
+                  />
+                );
+              })
+            : null}
+        </Styles.PostsSection>
+        <Styles.PaginationBox>
+          <Pagination switchPage={switchPage} pageCount={pageCount} />
+        </Styles.PaginationBox>
+      </Styles.MainSection>
     </>
   );
 };
--- src/components/Posts/styles.ts
+++ src/components/Posts/styles.ts
@@ -1,12 +1,66 @@
 import styled from "styled-components";
 
+export const MainSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: var(--default);
+  border: 1px solid blue;
+  width: 100%;
+`;
+
 export const PostsSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
   background-color: var(--default);
   border: 1px solid black;
   width: 100%;
-  height: 40em;
+  height: 85em;
+
+  @media (min-width: 700px) {
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    height: 45em;
+    max-width: 1000px;
+  }
+
+  @media (min-width: 1300px) {
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    height: 41em;
+    max-width: 1300px;
+  }
+`;
+
+export const PaginationBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 0 20px 0px;
+  .paginationUl {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0 10%;
+    /* @media (min-width: 1024px){
+        padding: 0 0;
+    } */
+  }
+  .previousBttn,
+  .nextBttn {
+    font-family: var(--title);
+    cursor: pointer;
+  }
+  .paginationActive {
+    font-family: var(--title);
+    text-decoration: underline;
+  }
+  .paginationPages {
+    cursor: pointer;
+  }
 `;

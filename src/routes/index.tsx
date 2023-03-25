e11bde42c02ec4d01c4b48c7b1fc0de6487b6576
kennedybm <kennedy_muniz@hotmail.com>
2023-03-25 20:46:54 -0300

    Feat: added params in posts route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import PostsDetails from "../pages/PostsDetails";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route/> */}
+        <Route exact path="/posts/:postId/:userId" component={PostsDetails} />
       </Switch>
     </Router>
   );

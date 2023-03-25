026b3718ba9b08534a665ec5b0883abdf31dbf95
kennedybm <kennedy_muniz@hotmail.com>
2023-03-25 20:45:36 -0300

    Fix: set position to be optinal

--- src/components/Header/interfaces.ts
+++ src/components/Header/interfaces.ts
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
 export interface IHeaderProps extends React.HTMLAttributes<HTMLHeadElement> {
-  position: boolean;
+  position?: boolean;
   disable?: boolean;
 }

96a8719aba41e73289a694d396b58c571812e78c
kennedybm <kennedy_muniz@hotmail.com>
2023-03-25 14:33:30 -0300

    Feat: created styles for secundary section and adding padding

--- src/components/Presentation/styles.ts
+++ src/components/Presentation/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const PresentationSection = styled.section`
+export const PresentationMainSection = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -9,28 +9,26 @@ export const PresentationSection = styled.section`
   height: 17em;
   width: 100%;
 `;
+export const PresentationSecundarySection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--decor);
+  height: 17em;
+  width: inherit;
+  padding-left: 1.3em;
+`;
 
 export const PresentationTitle = styled.div`
   display: flex;
   align-items: flex-end;
-  height: 40%;
   width: 100%;
+  height: 40%;
   h2 {
     font-size: var(--fs-xl);
     font-family: var(--title);
     font-weight: 600;
-    margin-left: 0.6em;
-  }
-  @media (min-width: 400px) {
-    h2 {
-      margin-bottom: 0.3em;
-    }
-  }
-  @media (min-width: 600px) {
-    h2 {
-      margin-left: 0.48em;
-      margin-bottom: 0.3em;
-    }
   }
 `;
 
@@ -41,7 +39,6 @@ export const PresentationDesc = styled.div`
     font-size: var(--fs-sml);
     font-family: var(--title);
     font-weight: 500;
-    margin-left: 1.3em;
     width: 77%;
     max-width: 20rem;
     display: -webkit-box;
@@ -50,10 +47,11 @@ export const PresentationDesc = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
   }
-  @media (min-width: 600px) {
+  @media (min-width: 1300px) {
     p {
-      font-size: 1rem;
-      max-width: 27rem;
+      font-size: 1.3rem;
+      -webkit-line-clamp: 5;
+      max-width: 30rem;
     }
   }
 `;

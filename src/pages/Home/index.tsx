28ff55c4aa0f03417587fc035c3dce631f628916
kennedybm <kennedy_muniz@hotmail.com>
2023-03-26 19:41:15 -0300

    Fix: making api integration and removing console.logs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,19 @@
 import * as Styles from "./styles";
 import { usePosts } from "../../providers/Api/Posts";
-import { useUsers } from "../../providers/Api/Users";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { Header, Presentaion, Posts } from "../../components";
 
 const Home = () => {
-  const { fetchPosts, fetchComments, postsData, commentsData } = usePosts();
-  const { usersData, setUsersData, fetchUsers } = useUsers();
+  const { fetchPosts } = usePosts();
 
   const [position, setPosition] = useState<boolean>(false);
 
-  const [postId, setPostId] = useState<number | undefined>();
-
-  // useEffect(() => {
-  //   fetchPosts();
-  // }, []);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
-    // console.log(scrollPosition);
     if (scrollPosition >= 206) {
       setPosition(!position);
     } else if (scrollPosition < 206) {
@@ -45,40 +40,3 @@ const Home = () => {
   );
 };
 export default Home;
-
-// useEffect(() => {
-//   fetchUsers();
-// }, []);
-
-// console.log(postsData);
-
-// const dataTest = [
-//   {
-//     id: 1,
-//     name: "olá",
-//   },
-//   {
-//     id: 2,
-//     name: "olá",
-//   },
-// ];
-
-// const handleId = (id: number) => {
-//   fetchComments(id);
-// };
-// console.log(usersData);
-
-{
-  /* <p>Teste Front End</p>
-
-      {usersData
-        ? usersData.map((item, index) => {
-            return (
-              <>
-                <h1 key={index}>{item.name}</h1>
-                <button onClick={() => handleId(item.id)}>Enviar</button>
-              </>
-            );
-          })
-        : null} */
-}

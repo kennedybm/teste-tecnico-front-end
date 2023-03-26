96222e52f502b0b270eb1b1ec8dd20ddb951f35b
kennedybm <kennedy_muniz@hotmail.com>
2023-03-26 19:40:15 -0300

    Feat: added Comments and MOdal components to exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,7 @@ import { Header } from "./Header";
 import { Presentaion } from "./Presentation";
 import { Posts } from "./Posts";
 import { Pagination } from "./Pagination";
+import { Comments } from "./Comments";
+import { Modal } from "./Modal";
 
-export { Header, Presentaion, Posts, Pagination };
+export { Header, Presentaion, Posts, Pagination, Comments, Modal };
